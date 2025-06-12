d1682e928922b55366919d73782a86186b833014
Rajesh Ranjan <sharmarajeshranjan2000@gmail.com>
2025-06-12 12:17:30 +0530

    Task

---
+++ findFirstBadVersion.js
@@ -0,0 +1,24 @@
+// Find First Bad Version – LeetCode #278
+
+
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1;
+        let right = n;
+
+        while (left < right) {
+            let mid = Math.floor(left + (right - left) / 2);
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1
+            }
+        }
+        return left;
+    };
+}; 
---
+++ searchInsertPosition.js
@@ -0,0 +1,19 @@
+// Search Insert Position – LeetCode #35
+
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1
+        }
+    }
+    return left;
+};
